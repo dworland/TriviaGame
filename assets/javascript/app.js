5c68cacc1d2dbf9ac7d50d9880ddf712fd672633
Devon Worland <devonworland@Devon-Worlands-MacBook-Pro.local>
2017-08-25 10:10:44 -0500

    added html and styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+var question1 = {
+	question: "Sequoia National Park is located in which U.S. state?",
+	answer1: "Oregon",
+	answer2: "Washington",
+	answer3: "Idaho",
+	answer4: "California",
+	correctAnswer: "California",
+}
+
+var question2 = {
+	question: "What is the largest ocean on planet Earth?",
+	answer1: "Atlantic",
+	answer2: "Pacific",
+	answer3: "Arctic",
+	answer4: "Indian",
+	correctAnswer: "Pacific",
+}
+
+var question3 = {
+	question: "What is the name of the longest mountain range in North America?",
+	answer1: "Rocky Mountains",
+	answer2: "Sierra Nevada",
+	answer3: "Appalachian Mountains",
+	answer4: "Great Smoky Mountains",
+	correctAnswer: "Rocky Mountains",
+}
+
+var question2 = {
+	question: "In which South American country would you find the ancient Incan citadel Machu Picchu?",
+	answer1: "Brazil",
+	answer2: "Peru",
+	answer3: "Argentina",
+	answer4: "Chile",
+	correctAnswer: "Peru",
+}
+
+var question2 = {
+	question: "Beirut is the capital and largest city of what country?",
+	answer1: "Germany",
+	answer2: "Portugal",
+	answer3: "Lebanon",
+	answer4: "Romania",
+	correctAnswer: "Lebanon",
+}
+
+// Player has a limited amount of time to finish the quiz
+
+// Show only one question until player answers, or time runs out
+
+// If player selects correct answer, show screen congratulating them
+
+// After a few seconds, display next question
+
+// If player runs out of time, tell them time is up, display correct answer
+
+// After a few seconds, display next question
+
+// If player chooses wrong answer, tell them they selected wrong option, display correct answer
+
+// After a few seconds, display next question
+
+// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
